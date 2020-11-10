f9259d1ed34bea923eb18126c62ef91405c030ba
Rimmert Zelle <rimmertzelle@rimmerts-mbp.home>
2020-11-10 13:08:12 +0100

    added answer folder

--- answer/app.ts
+++ answer/app.ts
@@ -13,6 +13,10 @@ class Game {
   private gameState: string;
   private winner: string;
 
+  // Car instances, one for each player
+  private car1: Car;
+  private car2: Car;
+
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext("2d");
@@ -21,6 +25,10 @@ class Game {
     this.canvas.height = window.innerHeight;
 
     this.keyboardListener = new KeyboardListener();
+
+    this.car1 = new Car("Bullet", "red", 100, 200);
+    this.car2 = new Car("Greek Arrow", "green", 100, 500);
+    console.log(this.car1);
 
     this.gameState = "begin";
 
@@ -40,6 +48,62 @@ class Game {
    * Based on the game state some actions have to be executed
    */
   private loop = () => {
+    if (this.gameState == "begin") {
+      this.writeTextToCanvas(
+        "Press R to Roll the dice",
+        30,
+        this.canvas.width / 2,
+        this.canvas.height - 50
+      );
+      this.writeTextToCanvas(
+        "Phase: begin",
+        40,
+        this.canvas.width / 2,
+        50,
+        "center",
+        "white"
+      );
+      if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_R)) {
+        this.gameState = "dice";
+        this.car1.distance = this.rollDice();
+        this.car2.distance = this.rollDice();
+      }
+    } else if (this.gameState == "dice") {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.writeTextToCanvas(
+        `Player 1: ${this.car1.distance}`,
+        40,
+        this.canvas.width / 2,
+        200,
+        "center",
+        "white"
+      );
+      this.writeTextToCanvas(
+        `Player 2: ${this.car2.distance}`,
+        40,
+        this.canvas.width / 2,
+        500,
+        "center",
+        "white"
+      );
+      this.gameState = "end";
+    } else if (this.gameState == "end") {
+      if (this.car1.distance > this.car2.distance) {
+        this.winner = this.car1.name;
+      } else if (this.car1.distance < this.car2.distance) {
+        this.winner = this.car2.name;
+      } else {
+        this.winner = "undecided";
+      }
+      this.writeTextToCanvas(
+        `Winner is ${this.winner}`,
+        60,
+        this.canvas.width / 2,
+        this.canvas.height - 50,
+        "center",
+        "red"
+      );
+    }
     this.draw();
     requestAnimationFrame(this.loop);
   };
@@ -48,6 +112,8 @@ class Game {
    * Function to draw all the cars on the canvas
    */
   private draw() {
+    this.car1.draw(this.ctx);
+    this.car2.draw(this.ctx);
   }
 
   /**
--- src/Car.ts
+++ src/Car.ts
@@ -1,47 +1,7 @@
 class Car {
-    private image: HTMLImageElement;
-    private _name: string;
-    private _distance: number;
-    private _xPosition: number;
-    private _yPosition: number;
+    constructor() {
 
-    constructor(name:string, colour:string, xPos:number, yPos:number) {
-        this._xPosition = xPos;
-        this._yPosition = yPos;
-        this._name = name;
-        this.image = this.loadNewImage(`./assets/img/${colour}-racing-car.png`);
-        //console.log(this.image);
     }
-
-    public set distance(distanceRaced : number) {
-        this._distance = distanceRaced;
-    }
-    
-    public get distance() : number {
-        return this._distance;
-    }
-
-    public get xPostition() : number {
-        return this._xPosition;
-    }
-
-    public get yPostition() : number {
-        return this._yPosition;
-    }
-
-    public get name() : string {
-        return this._name;
-    }
-    
-    /**
-    * Draw all the necessary items to the screen
-    */
-    public draw(ctx:CanvasRenderingContext2D) {
-        // draw player
-        //console.log(ctx);
-        ctx.drawImage(this.image, this._xPosition, this._yPosition);
-    }
-    
     /**
     * Method to load an image
     * @param {HTMLImageElement} source

25d4685678d8cec3b71721411563ebc3c9d22b2b
Rimmert Zelle <rimmertzelle@192.168.68.110>
2020-11-10 10:24:26 +0100

    added a working game

--- build/app.js
+++ build/app.js
@@ -1,22 +1,117 @@
-class Animal {
-    constructor(name, legs, sound) {
-        this.name = name;
-        this.legs = legs;
-        this.sound = sound;
+class Car {
+    constructor(name, colour, xPos, yPos) {
+        this._xPosition = xPos;
+        this._yPosition = yPos;
+        this._name = name;
+        this.image = this.loadNewImage(`./assets/img/${colour}-racing-car.png`);
     }
-    getName() {
-        return this.name;
+    set distance(distanceRaced) {
+        this._distance = distanceRaced;
     }
-    getLegs() {
-        return this.legs;
+    get distance() {
+        return this._distance;
     }
-    getSound() {
-        return this.sound;
+    get xPostition() {
+        return this._xPosition;
+    }
+    get yPostition() {
+        return this._yPosition;
+    }
+    get name() {
+        return this._name;
+    }
+    draw(ctx) {
+        ctx.drawImage(this.image, this._xPosition, this._yPosition);
+    }
+    loadNewImage(source) {
+        const img = new Image();
+        img.src = source;
+        return img;
     }
 }
-const animals = [
-    new Animal('dog', 4, 'woof'),
-    new Animal('cat', 4, 'meow')
-];
-animals.forEach((animal) => console.log('A %s has %s legs and goes %s!', animal.getName(), animal.getLegs(), animal.getSound()));
+class KeyboardListener {
+    constructor() {
+        this.keyDown = (ev) => {
+            this.keyCodeStates[ev.keyCode] = true;
+        };
+        this.keyUp = (ev) => {
+            this.keyCodeStates[ev.keyCode] = false;
+        };
+        this.keyCodeStates = new Array();
+        window.addEventListener("keydown", this.keyDown);
+        window.addEventListener("keyup", this.keyUp);
+    }
+    isKeyDown(keyCode) {
+        return this.keyCodeStates[keyCode] === true;
+    }
+}
+KeyboardListener.KEY_SPACE = 32;
+KeyboardListener.KEY_LEFT = 37;
+KeyboardListener.KEY_UP = 38;
+KeyboardListener.KEY_RIGHT = 39;
+KeyboardListener.KEY_DOWN = 40;
+KeyboardListener.KEY_R = 82;
+class Game {
+    constructor(canvas) {
+        this.loop = () => {
+            if (this.gameState == "begin") {
+                this.writeTextToCanvas("Press R to Roll the dice", 30, this.canvas.width / 2, this.canvas.height - 50);
+                this.writeTextToCanvas("Phase: begin", 40, this.canvas.width / 2, 50, "center", "white");
+                if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_R)) {
+                    this.gameState = "dice";
+                    this.car1.distance = this.rollDice();
+                    this.car2.distance = this.rollDice();
+                }
+            }
+            else if (this.gameState == "dice") {
+                this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                this.writeTextToCanvas(`Player 1: ${this.car1.distance}`, 40, this.canvas.width / 2, 200, "center", "white");
+                this.writeTextToCanvas(`Player 2: ${this.car2.distance}`, 40, this.canvas.width / 2, 500, "center", "white");
+                this.gameState = "end";
+            }
+            else if (this.gameState == "end") {
+                if (this.car1.distance > this.car2.distance) {
+                    this.winner = this.car1.name;
+                }
+                else if (this.car1.distance < this.car2.distance) {
+                    this.winner = this.car2.name;
+                }
+                else {
+                    this.winner = "undecided";
+                }
+                this.writeTextToCanvas(`Winner is ${this.winner}`, 60, this.canvas.width / 2, this.canvas.height - 50, "center", "red");
+            }
+            this.draw();
+            requestAnimationFrame(this.loop);
+        };
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.keyboardListener = new KeyboardListener();
+        this.car1 = new Car("Bullet", "red", 100, 200);
+        this.car2 = new Car("Greek Arrow", "green", 100, 500);
+        console.log(this.car1);
+        this.gameState = "begin";
+        this.loop();
+    }
+    rollDice() {
+        return this.randomNumber(1, 6);
+    }
+    draw() {
+        this.car1.draw(this.ctx);
+        this.car2.draw(this.ctx);
+    }
+    writeTextToCanvas(text, fontSize = 20, xCoordinate, yCoordinate, alignment = "center", color = "red") {
+        this.ctx.font = `${fontSize}px Minecraft`;
+        this.ctx.fillStyle = color;
+        this.ctx.textAlign = alignment;
+        this.ctx.fillText(text, xCoordinate, yCoordinate);
+    }
+    randomNumber(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+}
+let init = () => new Game(document.getElementById("canvas"));
+window.addEventListener("load", init);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,158 @@
-class Animal
-{
-    private readonly name: string
-    private readonly legs: number
-    private readonly sound: string
+/// <reference path="Car.ts" />
+/// <reference path="KeyboardListener.ts" />
 
-    public constructor(name: string, legs: number, sound: string)
-    {
-        this.name = name
-        this.legs = legs
-        this.sound = sound
-    }
+class Game {
+  // Necessary canvas attributes
+  private readonly canvas: HTMLCanvasElement;
+  private readonly ctx: CanvasRenderingContext2D;
 
-    public getName(): string
-    {
-        return this.name
-    }
+  // KeyboardListener so the player can move
+  private keyboardListener: KeyboardListener;
 
-    public getLegs(): number
-    {
-        return this.legs
-    }
+  // the state of the game: begin, dice and end
+  private gameState: string;
+  private winner: string;
 
-    public getSound(): string
-    {
-        return this.sound
+  // Car instances, one for each player
+  private car1: Car;
+  private car2: Car;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+
+    this.keyboardListener = new KeyboardListener();
+
+    this.car1 = new Car("Bullet", "red", 100, 200);
+    this.car2 = new Car("Greek Arrow", "green", 100, 500);
+    console.log(this.car1);
+
+    this.gameState = "begin";
+
+    this.loop();
+  }
+
+  /**
+   * Function to give a number between 1 and 6
+   * @returns {number} number - number between 1 and 6
+   */
+  private rollDice(): number {
+    return this.randomNumber(1, 6);
+  }
+
+  /**
+   * Method for the Game Loop
+   * Based on the game state some actions have to be executed
+   */
+  private loop = () => {
+    if (this.gameState == "begin") {
+      this.writeTextToCanvas(
+        "Press R to Roll the dice",
+        30,
+        this.canvas.width / 2,
+        this.canvas.height - 50
+      );
+      this.writeTextToCanvas(
+        "Phase: begin",
+        40,
+        this.canvas.width / 2,
+        50,
+        "center",
+        "white"
+      );
+      if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_R)) {
+        this.gameState = "dice";
+        this.car1.distance = this.rollDice();
+        this.car2.distance = this.rollDice();
+      }
+    } else if (this.gameState == "dice") {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.writeTextToCanvas(
+        `Player 1: ${this.car1.distance}`,
+        40,
+        this.canvas.width / 2,
+        200,
+        "center",
+        "white"
+      );
+      this.writeTextToCanvas(
+        `Player 2: ${this.car2.distance}`,
+        40,
+        this.canvas.width / 2,
+        500,
+        "center",
+        "white"
+      );
+      this.gameState = "end";
+    } else if (this.gameState == "end") {
+      if (this.car1.distance > this.car2.distance) {
+        this.winner = this.car1.name;
+      } else if (this.car1.distance < this.car2.distance) {
+        this.winner = this.car2.name;
+      } else {
+        this.winner = "undecided";
+      }
+      this.writeTextToCanvas(
+        `Winner is ${this.winner}`,
+        60,
+        this.canvas.width / 2,
+        this.canvas.height - 50,
+        "center",
+        "red"
+      );
     }
+    this.draw();
+    requestAnimationFrame(this.loop);
+  };
+
+  /**
+   * Function to draw all the cars on the canvas
+   */
+  private draw() {
+    this.car1.draw(this.ctx);
+    this.car2.draw(this.ctx);
+  }
+
+  /**
+   * Writes text to the canvas
+   * @param {string} text - Text to write
+   * @param {number} fontSize - Font size in pixels
+   * @param {number} xCoordinate - Horizontal coordinate in pixels
+   * @param {number} yCoordinate - Vertical coordinate in pixels
+   * @param {string} alignment - Where to align the text
+   * @param {string} color - The color of the text
+   */
+  public writeTextToCanvas(
+    text: string,
+    fontSize: number = 20,
+    xCoordinate: number,
+    yCoordinate: number,
+    alignment: CanvasTextAlign = "center",
+    color: string = "red"
+  ) {
+    this.ctx.font = `${fontSize}px Minecraft`;
+    this.ctx.fillStyle = color;
+    this.ctx.textAlign = alignment;
+    this.ctx.fillText(text, xCoordinate, yCoordinate);
+  }
+  /**
+   * Renders a random number between min and max
+   * @param {number} min - minimal time
+   * @param {number} max - maximal time
+   */
+  public randomNumber(min: number, max: number): number {
+    return Math.round(Math.random() * (max - min) + min);
+  }
 }
 
-const animals = [
-     new Animal('dog', 4, 'woof'),
-     new Animal('cat', 4, 'meow')
-]
+/**
+ * Start the game whenever the entire DOM is loaded
+ */
+let init = () =>
+  new Game(document.getElementById("canvas") as HTMLCanvasElement);
 
-animals.forEach(
-    (animal) => console.log(
-        'A %s has %s legs and goes %s!',
-        animal.getName(),
-        animal.getLegs(),
-        animal.getSound()
-    )
-)
+// Add EventListener to load the game whenever the browser is ready
+window.addEventListener("load", init);
